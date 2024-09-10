8ce7deceaab2313d4a37f606a0fb74493a519ea7
yusuf u <yusuf.uzan>
2024-09-10 22:27:11 +0300

    Fi

--- Package.swift
+++ Package.swift
@@ -12,10 +12,6 @@ let package = Package(
       name: "SwiftPlus",
       targets: ["SwiftPlus"]),
   ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-     .package(name: "SwiftyKeychainKit", url: "https://github.com/andriyslyusar/SwiftyKeychainKit", branch: "master")
-  ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
